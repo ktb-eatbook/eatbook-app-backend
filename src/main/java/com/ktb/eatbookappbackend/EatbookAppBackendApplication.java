420a195bc2431e3a5f6dee9855b7a2bec2a0561a
cjm0611 <cjm-0611@naver.com>
2024-10-23 15:44:25 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/main/java/com/ktb/eatbookappbackend/EatbookAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.eatbookappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatbookAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatbookAppBackendApplication.class, args);
+    }
+
+}
