d01b6ac399790cf67df26f1c897f5e94c62da583
cjm0611 <cjm-0611@naver.com>
2024-10-29 14:26:43 +0900

    feat: domain 전반에 사용되는 성공, 실패 response dto 생성

--- src/main/java/com/ktb/eatbookappbackend/domain/global/reponse/FailureResponseDTO.java
+++ src/main/java/com/ktb/eatbookappbackend/domain/global/reponse/FailureResponseDTO.java
@@ -1,10 +1,16 @@
 package com.ktb.eatbookappbackend.domain.global.reponse;
 
+import com.ktb.eatbookappbackend.domain.global.message.MessageCode;
+
 public record FailureResponseDTO (
         int statusCode,
         String message,
         String subStatus
 ) {
+    public static FailureResponseDTO of(MessageCode errorMessage) {
+        return new FailureResponseDTO(errorMessage.getStatus().value(), errorMessage.getMessage(), null);
+    }
+
     public static FailureResponseDTO of(int statusCode, String message) {
         return new FailureResponseDTO(statusCode, message, null);
     }
