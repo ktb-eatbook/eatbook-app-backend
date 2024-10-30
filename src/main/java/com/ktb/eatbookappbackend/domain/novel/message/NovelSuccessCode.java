967606134dfd8d7b63f16ac0d4b4b4af240c4235
cjm0611 <cjm-0611@naver.com>
2024-10-30 17:30:44 +0900

    feat: 소설 도메인 success response의 message, code를 관리하는 enum 생성

---
+++ src/main/java/com/ktb/eatbookappbackend/domain/novel/message/NovelSuccessCode.java
@@ -0,0 +1,18 @@
+package com.ktb.eatbookappbackend.domain.novel.message;
+
+import com.ktb.eatbookappbackend.domain.global.message.MessageCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum NovelSuccessCode implements MessageCode {
+    NOVEL_DETAIL_RETRIEVED("성공적으로 소설 정보와 에피소드 목록을 조회했습니다.",HttpStatus.OK);
+
+    private final String message;
+    private final HttpStatus status;
+
+    NovelSuccessCode(final String message, final HttpStatus status) {
+        this.message = message;
+        this.status = status;
+    }
+}
