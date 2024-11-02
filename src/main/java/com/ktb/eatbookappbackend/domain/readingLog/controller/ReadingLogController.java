15429bdca6f2d7c185225e566918a2c17bccdaa3
cjm0611 <cjm-0611@naver.com>
2024-11-02 18:13:02 +0900

    fix: 독서 기록 조회성공한 경우 적절한 message 반환

--- src/main/java/com/ktb/eatbookappbackend/domain/readingLog/controller/ReadingLogController.java
+++ src/main/java/com/ktb/eatbookappbackend/domain/readingLog/controller/ReadingLogController.java
@@ -5,6 +5,7 @@ import com.ktb.eatbookappbackend.domain.global.authentication.AuthenticationAspe
 import com.ktb.eatbookappbackend.domain.global.message.GlobalSuccessMessage;
 import com.ktb.eatbookappbackend.domain.global.reponse.SuccessResponseDTO;
 import com.ktb.eatbookappbackend.domain.novel.dto.ContinueReadingInfoDTO;
+import com.ktb.eatbookappbackend.domain.readingLog.message.ReadingLogSuccessCode;
 import com.ktb.eatbookappbackend.domain.readingLog.service.ReadingLogService;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,13 +28,9 @@ public class ReadingLogController {
      * 인증된 멤버의 소설의 마지막으로 읽은 에피소드 정보를 가져옵니다.
      *
      * @param novelId 소설의 고유 식별자.
-     * @return ResponseEntity로 마지막으로 읽은 에피소드 정보를 포함하거나, 결과가 없음을 나타내는 성공 메시지를 반환합니다.
-     *         응답 본문은 다음 구조를 갖는 SuccessResponseDTO입니다:
-     *         - success: 성공 여부를 나타내는 boolean.
-     *         - message: 성공 메시지를 나타내는 string.
-     *         - data: 다음 key-value 쌍을 포함하는 map:
-     *           - continueReadingInfo: Optional로 ContinueReadingInfoDTO를 나타내는 마지막으로 읽은 에피소드 정보.
-     *             마지막으로 읽은 에피소드가 없는 경우, 이 필드는 null이 됩니다.
+     * @return ResponseEntity로 마지막으로 읽은 에피소드 정보를 포함하거나, 결과가 없음을 나타내는 성공 메시지를 반환합니다. 응답 본문은 다음 구조를 갖는 SuccessResponseDTO입니다: - success: 성공
+     * 여부를 나타내는 boolean. - message: 성공 메시지를 나타내는 string. - data: 다음 key-value 쌍을 포함하는 map: - continueReadingInfo: Optional로
+     * ContinueReadingInfoDTO를 나타내는 마지막으로 읽은 에피소드 정보. 마지막으로 읽은 에피소드가 없는 경우, 이 필드는 null이 됩니다.
      */
     @Authenticated
     @GetMapping("/novels/{novelId}/last-read")
@@ -48,6 +45,6 @@ public class ReadingLogController {
 
         Map<String, Object> data = new HashMap<>();
         data.put("continueReadingInfo", lastReadEpisodeOpt.get());
-        return ResponseEntity.ok(SuccessResponseDTO.of(GlobalSuccessMessage.NO_RESULTS_FOUND, data));
+        return ResponseEntity.ok(SuccessResponseDTO.of(ReadingLogSuccessCode.BOOKMARKS_RETRIEVED, data));
     }
 }
