50caae71edaefdc1e2ab876e0fe4a0b56978f148
cjm0611 <cjm-0611@naver.com>
2024-11-04 17:36:36 +0900

    fix: 유저 에러 메세지 수정

--- src/main/java/com/ktb/eatbookappbackend/domain/member/message/MemberErrorCode.java
+++ src/main/java/com/ktb/eatbookappbackend/domain/member/message/MemberErrorCode.java
@@ -6,8 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum MemberErrorCode implements MessageCode {
-    PAPER_NOT_FOUND("요청하신 페이퍼를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    PAPER_CREATION_FAILED("페이지 생성에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    MEMBER_NOT_FOUND("요청하신 회원을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus status;
