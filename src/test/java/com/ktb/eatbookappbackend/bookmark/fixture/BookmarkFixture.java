42890a86a66a38afa9f93e0cdb30502440564eb7
cjm0611 <cjm-0611@naver.com>
2024-11-05 16:10:28 +0900

    test: bookmark id 부여하는 로직 삭제

--- src/test/java/com/ktb/eatbookappbackend/bookmark/fixture/BookmarkFixture.java
+++ src/test/java/com/ktb/eatbookappbackend/bookmark/fixture/BookmarkFixture.java
@@ -3,18 +3,13 @@ package com.ktb.eatbookappbackend.bookmark.fixture;
 import com.ktb.eatbookappbackend.entity.Bookmark;
 import com.ktb.eatbookappbackend.entity.Member;
 import com.ktb.eatbookappbackend.entity.Novel;
-import java.util.UUID;
-import org.springframework.test.util.ReflectionTestUtils;
 
 public class BookmarkFixture {
 
     public static Bookmark createBookmark(Member member, Novel novel) {
-        Bookmark bookmark = Bookmark.builder()
+        return Bookmark.builder()
             .member(member)
             .novel(novel)
             .build();
-        String bookmarkId = UUID.randomUUID().toString();
-        ReflectionTestUtils.setField(novel, "id", bookmarkId);
-        return bookmark;
     }
 }
