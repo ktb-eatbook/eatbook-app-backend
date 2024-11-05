4c424dfc18583003e0bfceccbc9150608df30ccf
cjm0611 <cjm-0611@naver.com>
2024-11-05 10:35:05 +0900

    feat: EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/ktb/eatbookappbackend/EatbookAppBackendApplication.java
+++ src/main/java/com/ktb/eatbookappbackend/EatbookAppBackendApplication.java
@@ -2,12 +2,13 @@ package com.ktb.eatbookappbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EatbookAppBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EatbookAppBackendApplication.class, args);
     }
-
 }
