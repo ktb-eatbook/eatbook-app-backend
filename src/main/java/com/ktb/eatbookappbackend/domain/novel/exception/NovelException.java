08e9786f2186590e9538a05001997948487df186
cjm0611 <cjm-0611@naver.com>
2024-11-05 09:51:06 +0900

    feat: 소설 에러 핸들러 추가

--- src/main/java/com/ktb/eatbookappbackend/domain/novel/exception/NovelException.java
+++ src/main/java/com/ktb/eatbookappbackend/domain/novel/exception/NovelException.java
@@ -2,10 +2,15 @@ package com.ktb.eatbookappbackend.domain.novel.exception;
 
 import com.ktb.eatbookappbackend.domain.global.exception.DomainException;
 import com.ktb.eatbookappbackend.domain.novel.message.NovelErrorCode;
+import lombok.Getter;
 
+@Getter
 public class NovelException extends DomainException {
+
+    private final NovelErrorCode errorCode;
 
     public NovelException(NovelErrorCode errorCode) {
         super(errorCode);
+        this.errorCode = errorCode;
     }
 }
