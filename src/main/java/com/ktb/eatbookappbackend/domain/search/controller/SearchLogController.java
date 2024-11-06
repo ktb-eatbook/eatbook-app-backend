e77d4104b136eb2800b6bce5a196d0e531d96f6d
cjm0611 <cjm-0611@naver.com>
2024-11-06 16:58:58 +0900

    docs: 독서 기록을 생성하는 API JavaDocs 추가

--- src/main/java/com/ktb/eatbookappbackend/domain/search/controller/SearchLogController.java
+++ src/main/java/com/ktb/eatbookappbackend/domain/search/controller/SearchLogController.java
@@ -22,6 +22,14 @@ public class SearchLogController {
 
     private final SearchLogService searchService;
 
+    /**
+     * 이 메소드는 검색 로그 생성 요청을 처리합니다. {@link SearchLogRequest} 객체를 받아 검색어를 포함하고, {@link SearchLogService}를 사용하여 해당 검색어에 해당하는
+     * {@link SearchLogDTO} 객체를 생성합니다. 그런 다음, {@link ResponseEntity}를 반환하며, 성공 코드와 생성된 {@link SearchLogResponse}를 포함하는
+     * {@link SuccessResponseDTO}를 담습니다.
+     *
+     * @param searchLogRequest 검색어를 포함하는 요청 객체.
+     * @return {@link SearchLogResponse}를 포함하는 성공 코드가 있는 {@link SuccessResponseDTO}를 포함하는 {@link ResponseEntity}.
+     */
     @PostMapping()
     public ResponseEntity<SuccessResponseDTO> createSearchLog(@RequestBody SearchLogRequest searchLogRequest) {
         SearchLogDTO searchLog = searchService.createSearchLog(searchLogRequest.term());
