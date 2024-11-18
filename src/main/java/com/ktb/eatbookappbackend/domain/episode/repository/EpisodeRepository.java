83bb98e9323e2a8764712247eb3ef8672d07a3cb
cjm0611 <cjm-0611@naver.com>
2024-11-18 13:42:02 +0900

    refactor: 사용하지 않는 repository method 삭제

--- src/main/java/com/ktb/eatbookappbackend/domain/episode/repository/EpisodeRepository.java
+++ src/main/java/com/ktb/eatbookappbackend/domain/episode/repository/EpisodeRepository.java
@@ -1,13 +1,8 @@
 package com.ktb.eatbookappbackend.domain.episode.repository;
 
 import com.ktb.eatbookappbackend.entity.Episode;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface EpisodeRepository extends JpaRepository<Episode, String> {
 
-    @Query("SELECT e FROM Episode e WHERE e.novel.id = :novelId AND e.releasedDate IS NOT NULL ORDER BY e.releasedDate DESC")
-    List<Episode> findLatestEpisodesByNovel(@Param("novelId") String novelId);
 }
