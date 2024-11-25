736ac55cd607269e9bc926ac4f8ff04a32a517b0
cjm0611 <cjm-0611@naver.com>
2024-11-25 13:50:27 +0900

    feat: 쿠키 생성 시 sameSite, secure

--- src/main/java/com/ktb/eatbookappbackend/oauth/jwt/CookieService.java
+++ src/main/java/com/ktb/eatbookappbackend/oauth/jwt/CookieService.java
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpHeaders.SET_COOKIE;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +14,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class CookieService {
 
+    @Value("${app.properties.cookieSameSite}")
+    private String cookieSameSite;
+
+    @Value("${app.properties.cookieSecure}")
+    private boolean cookieSecure;
+
     // TO DO - secure, domain, sameSite 설정 추가
     public ResponseCookie createCookie(String cookieName, String cookieValue) {
         return ResponseCookie.from(cookieName, cookieValue)
             .path("/")
+            .sameSite(cookieSameSite)
             .httpOnly(true)
+            .secure(cookieSecure)
             .build();
     }
 
