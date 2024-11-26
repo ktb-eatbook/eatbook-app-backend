cc4cbf466349523020333d4de4e2d031c78c9ca6
cjm0611 <cjm-0611@naver.com>
2024-11-26 17:09:03 +0900

    feat: 회원가입 컨트롤러 반환값 변경

--- src/main/java/com/ktb/eatbookappbackend/oauth/controller/SignupController.java
+++ src/main/java/com/ktb/eatbookappbackend/oauth/controller/SignupController.java
@@ -1,30 +1,54 @@
 package com.ktb.eatbookappbackend.oauth.controller;
 
+import static com.ktb.eatbookappbackend.oauth.jwt.constant.TokenType.ACCESS_TOKEN;
+import static com.ktb.eatbookappbackend.oauth.jwt.constant.TokenType.REFRESH_TOKEN;
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
+
 import com.ktb.eatbookappbackend.entity.Member;
 import com.ktb.eatbookappbackend.oauth.dto.SignupRequestDTO;
 import com.ktb.eatbookappbackend.oauth.dto.SignupResponseDTO;
+import com.ktb.eatbookappbackend.oauth.jwt.CookieService;
 import com.ktb.eatbookappbackend.oauth.jwt.JwtUtil;
 import com.ktb.eatbookappbackend.oauth.service.SignupService;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/signup")
 public class SignupController {
 
     private final JwtUtil jwtUtil;
     private final SignupService signupService;
+    private final CookieService cookieService;
 
-    @PostMapping("/signup")
+    @GetMapping("/additional-info")
+    public String loginResult(@RequestParam("token") String token) {
+        String state = "SIGN_UP";
+        return String.format("""
+            <html>
+              <body>
+                <script>
+                  window.flutter_inappwebview.callHandler('loginResult', { state: '%s', token: '%s' });
+                </script>
+                <p>로그인 처리 중</p>
+              </body>
+            </html>
+            """, state, token);
+    }
+
+    @PostMapping()
     public ResponseEntity<SignupResponseDTO> getSignupInfo(@RequestBody SignupRequestDTO request) {
         String token = request.token();
 
@@ -33,7 +57,6 @@ public class SignupController {
         }
 
         Map<String, String> signupInfo = jwtUtil.extractSignupClaims(token);
-        log.info("signupInfo: " + signupInfo);
         Member newMember = signupService.createMember(
             signupInfo.get("email"),
             signupInfo.get("nickname"),
@@ -42,13 +65,22 @@ public class SignupController {
             request.getAgeGroupEnum()
         );
 
-        SignupResponseDTO responseDTO = new SignupResponseDTO(
+        SignupResponseDTO signupResponseDTO = new SignupResponseDTO(
             newMember.getId(),
             newMember.getNickname(),
             newMember.getProfileImageUrl(),
             newMember.getEmail()
         );
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+        String accessToken = jwtUtil.generateAccessToken(newMember.getId(), newMember.getRole());
+        String refreshToken = jwtUtil.generateRefreshToken(newMember.getId());
+        ResponseCookie accessTokenCookie = cookieService.createCookie(ACCESS_TOKEN.getValue(), accessToken);
+        ResponseCookie refreshTokenCookie = cookieService.createCookie(REFRESH_TOKEN.getValue(), refreshToken);
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .header(SET_COOKIE, accessTokenCookie.toString())
+            .header(SET_COOKIE, refreshTokenCookie.toString())
+            .body(signupResponseDTO);
     }
 }
