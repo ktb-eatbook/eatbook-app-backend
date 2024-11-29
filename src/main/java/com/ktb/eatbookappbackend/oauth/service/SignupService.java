03c748798ea54f7cdbf0bbbacf31681049661676
cjm0611 <cjm-0611@naver.com>
2024-11-29 12:07:56 +0900

    feat: 회원가입 시 유저세팅 생성

--- src/main/java/com/ktb/eatbookappbackend/oauth/service/SignupService.java
+++ src/main/java/com/ktb/eatbookappbackend/oauth/service/SignupService.java
@@ -1,20 +1,36 @@
 package com.ktb.eatbookappbackend.oauth.service;
 
 import com.ktb.eatbookappbackend.domain.member.repository.MemberRepository;
+import com.ktb.eatbookappbackend.domain.memberSetting.repository.MemberSettingRepository;
 import com.ktb.eatbookappbackend.entity.Member;
+import com.ktb.eatbookappbackend.entity.MemberSetting;
 import com.ktb.eatbookappbackend.entity.constant.AgeGroup;
 import com.ktb.eatbookappbackend.entity.constant.Gender;
+import com.ktb.eatbookappbackend.oauth.dto.MemberDTO;
+import com.ktb.eatbookappbackend.oauth.dto.MemberSettingDTO;
+import com.ktb.eatbookappbackend.oauth.dto.SignupResponseDTO;
 import com.ktb.eatbookappbackend.oauth.exception.SignupException;
 import com.ktb.eatbookappbackend.oauth.message.SignupErrorCode;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class SignupService {
 
     private final MemberRepository memberRepository;
+    private final MemberSettingRepository memberSettingRepository;
 
+    public SignupResponseDTO signUp(String email, String nickname, String profileImageUrl, Gender gender, AgeGroup ageGroup) {
+        Member newMember = createMember(email, nickname, profileImageUrl, gender, ageGroup);
+        MemberSettingDTO memberSettingDTO = MemberSettingDTO.of(newMember.getMemberSetting());
+        MemberDTO memberDTO = MemberDTO.of(newMember, List.of(), List.of(), memberSettingDTO);
+        return SignupResponseDTO.of(memberDTO);
+    }
+
+    @Transactional
     public Member createMember(String email, String nickname, String profileImageUrl, Gender gender, AgeGroup ageGroup) {
         if (memberRepository.existsByEmail(email)) {
             throw new SignupException(SignupErrorCode.EMAIL_DUPLICATED);
@@ -28,6 +44,15 @@ public class SignupService {
             .ageGroup(ageGroup)
             .build();
 
-        return memberRepository.save(member);
+        memberRepository.save(member);
+
+        MemberSetting memberSetting = MemberSetting.builder()
+            .member(member)
+            .build();
+
+        memberSettingRepository.save(memberSetting);
+        member.updateMemberSetting(memberSetting);
+
+        return member;
     }
 }
