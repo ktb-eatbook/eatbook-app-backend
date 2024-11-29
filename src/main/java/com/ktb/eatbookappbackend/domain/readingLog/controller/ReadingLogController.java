1785c0e2889bec2ada8bbdfa45c56ade1cb16073
cjm0611 <cjm-0611@naver.com>
2024-11-29 12:46:07 +0900

    feat: 독서 기록 조회 시 유저 인증

--- src/main/java/com/ktb/eatbookappbackend/domain/readingLog/controller/ReadingLogController.java
+++ src/main/java/com/ktb/eatbookappbackend/domain/readingLog/controller/ReadingLogController.java
@@ -1,15 +1,17 @@
 package com.ktb.eatbookappbackend.domain.readingLog.controller;
 
-import com.ktb.eatbookappbackend.global.authentication.Authenticated;
-import com.ktb.eatbookappbackend.global.authentication.AuthenticationAspect;
-import com.ktb.eatbookappbackend.global.message.GlobalSuccessMessage;
-import com.ktb.eatbookappbackend.global.reponse.SuccessResponseDTO;
 import com.ktb.eatbookappbackend.domain.novel.dto.ContinueReadingInfoDTO;
 import com.ktb.eatbookappbackend.domain.readingLog.message.ReadingLogSuccessCode;
 import com.ktb.eatbookappbackend.domain.readingLog.service.ReadingLogService;
+import com.ktb.eatbookappbackend.entity.constant.Role;
+import com.ktb.eatbookappbackend.global.authentication.Authenticated;
+import com.ktb.eatbookappbackend.global.message.GlobalSuccessMessage;
+import com.ktb.eatbookappbackend.global.reponse.SuccessResponseDTO;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,11 +32,13 @@ public class ReadingLogController {
      * 여부를 나타내는 boolean. - message: 성공 메시지를 나타내는 string. - data: 다음 key-value 쌍을 포함하는 map: - continueReadingInfo: Optional로
      * ContinueReadingInfoDTO를 나타내는 마지막으로 읽은 에피소드 정보. 마지막으로 읽은 에피소드가 없는 경우, 이 필드는 null이 됩니다.
      */
+    @Secured(Role.MEMBER_VALUE)
     @Authenticated
     @GetMapping("/novels/{novelId}/last-read")
-    public ResponseEntity<?> getLastReadEpisode(@PathVariable("novelId") String novelId) {
-        String memberId = AuthenticationAspect.getAuthenticatedMemberId();
-
+    public ResponseEntity<?> getLastReadEpisode(
+        @PathVariable("novelId") String novelId,
+        @AuthenticationPrincipal String memberId
+    ) {
         Optional<ContinueReadingInfoDTO> lastReadEpisode = readingLogService.getLastReadEpisode(memberId, novelId);
 
         if (lastReadEpisode.isEmpty()) {
