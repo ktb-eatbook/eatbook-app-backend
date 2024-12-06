4af9df58628f70433e7eaa6b0c6bcb3d30472ec3
cjm0611 <cjm-0611@naver.com>
2024-12-06 09:54:05 +0900

    style: docs indent 변경

--- src/main/java/com/ktb/eatbookappbackend/oauth/service/AuthService.java
+++ src/main/java/com/ktb/eatbookappbackend/oauth/service/AuthService.java
@@ -30,11 +30,11 @@ public class AuthService {
     /**
      * 사용자 가입 프로세스를 처리합니다.
      *
-     * @param email 사용자의 이메일. 반드시 고유하고 null이 아니어야 합니다.
-     * @param nickname 사용자의 닉네임. null이 아니어야 합니다.
+     * @param email           사용자의 이메일. 반드시 고유하고 null이 아니어야 합니다.
+     * @param nickname        사용자의 닉네임. null이 아니어야 합니다.
      * @param profileImageUrl 사용자의 프로필 이미지 URL. null일 수 있습니다.
-     * @param gender 사용자의 성별. null이 아니어야 합니다.
-     * @param ageGroup 사용자의 연령대. null이 아니어야 합니다.
+     * @param gender          사용자의 성별. null이 아니어야 합니다.
+     * @param ageGroup        사용자의 연령대. null이 아니어야 합니다.
      * @return {@link SignupResponseDTO}로, 가입한 사용자의 정보를 포함합니다.
      * @throws SignupException 이메일이 이미 등록된 경우 발생합니다.
      */
@@ -49,9 +49,7 @@ public class AuthService {
      * 로그인 프로세스를 처리합니다.
      *
      * @param member 로그인을 시도하는 사용자. null이 아니어야 합니다.
-     * @return {@link MemberDTO}로, 로그인한 사용자의 정보를 포함합니다.
-     *         사용자의 북마크한 소설 ID, 좋아요한 소설 ID,
-     *         사용자 설정 정보를 포함합니다.
+     * @return {@link MemberDTO}로, 로그인한 사용자의 정보를 포함합니다. 사용자의 북마크한 소설 ID, 좋아요한 소설 ID, 사용자 설정 정보를 포함합니다.
      */
     public MemberDTO login(Member member) {
         MemberSettingDTO memberSetting = MemberSettingDTO.of(member.getMemberSetting());
@@ -63,11 +61,11 @@ public class AuthService {
     /**
      * 시스템에 새로운 멤버를 생성합니다.
      *
-     * @param email 멤버의 고유 이메일. null이 아니어야 합니다.
-     * @param nickname 멤버의 닉네임. null이 아니어야 합니다.
+     * @param email           멤버의 고유 이메일. null이 아니어야 합니다.
+     * @param nickname        멤버의 닉네임. null이 아니어야 합니다.
      * @param profileImageUrl 멤버의 프로필 이미지 URL. null일 수 있습니다.
-     * @param gender 멤버의 성별. null이 아니어야 합니다.
-     * @param ageGroup 멤버의 연령대. null이 아니어야 합니다.
+     * @param gender          멤버의 성별. null이 아니어야 합니다.
+     * @param ageGroup        멤버의 연령대. null이 아니어야 합니다.
      * @return 새로 생성된 멤버.
      * @throws SignupException 이메일이 이미 시스템에 등록된 경우 발생합니다.
      */
