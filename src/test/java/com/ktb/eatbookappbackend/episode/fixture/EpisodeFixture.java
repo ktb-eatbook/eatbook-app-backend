d60b4bc4e9f646826ea2af0e73b1252b69e24ff9
cjm0611 <cjm-0611@naver.com>
2024-12-06 13:23:11 +0900

    test: get presigned url에 대한 테스트 코드 추가

--- src/test/java/com/ktb/eatbookappbackend/episode/fixture/EpisodeFixture.java
+++ src/test/java/com/ktb/eatbookappbackend/episode/fixture/EpisodeFixture.java
@@ -1,8 +1,10 @@
 package com.ktb.eatbookappbackend.episode.fixture;
 
 import com.ktb.eatbookappbackend.entity.Episode;
+import com.ktb.eatbookappbackend.entity.FileMetadata;
 import com.ktb.eatbookappbackend.entity.Novel;
 import com.ktb.eatbookappbackend.entity.constant.EpisodeReleaseStatus;
+import com.ktb.eatbookappbackend.entity.constant.FileType;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -23,4 +25,28 @@ public class EpisodeFixture {
         ReflectionTestUtils.setField(episode, "id", episodeID);
         return episode;
     }
+
+    public static Episode createEpisode() {
+        Episode episode = Episode.builder()
+            .title("Episode Title")
+            .chapterNumber(1)
+            .releasedDate(LocalDateTime.now())
+            .releaseStatus(EpisodeReleaseStatus.PUBLIC)
+            .build();
+        String episodeID = UUID.randomUUID().toString();
+        ReflectionTestUtils.setField(episode, "id", episodeID);
+        return episode;
+    }
+
+    public static FileMetadata createFileMetadata(FileType type, String path, Episode episode) {
+        FileMetadata metadata = FileMetadata.builder()
+            .type(type)
+            .path(path)
+            .episode(episode)
+            .build();
+        String fileMetadataId = UUID.randomUUID().toString();
+        ReflectionTestUtils.setField(episode, "id", fileMetadataId);
+        return metadata;
+    }
 }
+
