6869d297e654751256bfda45a2821d7762841ec5
cjm0611 <cjm-0611@naver.com>
2024-12-06 15:33:55 +0900

    refactor: 파일 형식 오류의 http status code 변경

--- src/main/java/com/ktb/eatbookappbackend/domain/episode/message/EpisodeFileErrorCode.java
+++ src/main/java/com/ktb/eatbookappbackend/domain/episode/message/EpisodeFileErrorCode.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 public enum EpisodeFileErrorCode implements MessageCode {
     PRESIGNED_URL_GENERATION_FAILED("Presigned URL 생성 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     FILE_NOT_FOUND("요청하신 파일을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    FILE_TYPE_NOT_FOUND("지원되지 않는 파일 형식입니다.", HttpStatus.NOT_FOUND);
+    FILE_TYPE_NOT_FOUND("지원되지 않는 파일 형식입니다.", HttpStatus.BAD_REQUEST);
 
     private final String message;
     private final HttpStatus status;
