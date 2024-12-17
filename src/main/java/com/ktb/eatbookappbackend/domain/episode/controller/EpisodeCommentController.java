e6ba93902c805444c804bcb0d099ecfef34d518f
cjm0611 <cjm-0611@naver.com>
2024-12-17 09:33:08 +0900

    feat: 댓글 조회, 생성, 삭제 API

--- src/main/java/com/ktb/eatbookappbackend/domain/episode/controller/EpisodeCommentController.java
+++ src/main/java/com/ktb/eatbookappbackend/domain/episode/controller/EpisodeCommentController.java
@@ -1,14 +1,21 @@
 package com.ktb.eatbookappbackend.domain.episode.controller;
 
+import com.ktb.eatbookappbackend.domain.episode.dto.CommentDTO;
 import com.ktb.eatbookappbackend.domain.episode.dto.EpisodeCommentsDTO;
-import com.ktb.eatbookappbackend.domain.episode.message.EpisodeCommentSuccessCode;
+import com.ktb.eatbookappbackend.domain.episode.message.EpisodeSuccessCode;
 import com.ktb.eatbookappbackend.domain.episode.service.EpisodeCommentService;
+import com.ktb.eatbookappbackend.entity.constant.Role;
 import com.ktb.eatbookappbackend.global.reponse.SuccessResponse;
 import com.ktb.eatbookappbackend.global.reponse.SuccessResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +29,27 @@ public class EpisodeCommentController {
     @GetMapping
     public ResponseEntity<SuccessResponseDTO> getComments(@PathVariable("episodeId") String episodeId) {
         EpisodeCommentsDTO episodeCommentsDTO = episodeCommentService.getCommentsByEpisodeId(episodeId);
-        return SuccessResponse.toResponseEntity(EpisodeCommentSuccessCode.COMMENTS_RETRIEVED, episodeCommentsDTO);
+        return SuccessResponse.toResponseEntity(EpisodeSuccessCode.COMMENTS_RETRIEVED, episodeCommentsDTO);
+    }
+
+    @Secured(Role.MEMBER_VALUE)
+    @PostMapping
+    public ResponseEntity<SuccessResponseDTO> createComment(
+        @PathVariable("episodeId") String episodeId,
+        @AuthenticationPrincipal String memberId,
+        @RequestBody EpisodeCommentRequestDTO request
+    ) {
+        CommentDTO comment = episodeCommentService.createComment(episodeId, memberId, request);
+        return SuccessResponse.toResponseEntity(EpisodeSuccessCode.COMMENT_CREATED, comment);
+    }
+
+    @Secured(Role.MEMBER_VALUE)
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<SuccessResponseDTO> deleteComment(
+        @PathVariable("episodeId") String episodeId,
+        @PathVariable("commentId") String commentId
+    ) {
+        episodeCommentService.deleteComment(commentId);
+        return SuccessResponse.toResponseEntity(EpisodeSuccessCode.COMMENT_DELETED);
     }
 }
--- src/main/java/com/ktb/eatbookappbackend/domain/episode/service/EpisodeCommentService.java
+++ src/main/java/com/ktb/eatbookappbackend/domain/episode/service/EpisodeCommentService.java
@@ -1,9 +1,16 @@
 package com.ktb.eatbookappbackend.domain.episode.service;
 
 import com.ktb.eatbookappbackend.domain.comment.repository.CommentRepository;
+import com.ktb.eatbookappbackend.domain.episode.controller.EpisodeCommentRequestDTO;
 import com.ktb.eatbookappbackend.domain.episode.dto.CommentDTO;
 import com.ktb.eatbookappbackend.domain.episode.dto.EpisodeCommentsDTO;
+import com.ktb.eatbookappbackend.domain.episode.exception.EpisodeException;
+import com.ktb.eatbookappbackend.domain.episode.message.EpisodeErrorCode;
+import com.ktb.eatbookappbackend.domain.episode.repository.EpisodeRepository;
+import com.ktb.eatbookappbackend.domain.member.service.MemberService;
 import com.ktb.eatbookappbackend.entity.Comment;
+import com.ktb.eatbookappbackend.entity.Episode;
+import com.ktb.eatbookappbackend.entity.Member;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +21,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class EpisodeCommentService {
 
     private final CommentRepository commentRepository;
+    private final EpisodeRepository episodeRepository;
+    private final MemberService memberService;
 
     @Transactional(readOnly = true)
     public EpisodeCommentsDTO getCommentsByEpisodeId(String episodeId) {
@@ -28,5 +37,30 @@ public class EpisodeCommentService {
             .toList();
 
         return EpisodeCommentsDTO.of(commentDTOs);
+    }
+
+    @Transactional
+    public CommentDTO createComment(String episodeId, String memberId, EpisodeCommentRequestDTO request) {
+        Episode episode = episodeRepository.findById(episodeId).orElseThrow(() -> new EpisodeException(EpisodeErrorCode.EPISODE_NOT_FOUND));
+        Member member = memberService.findById(memberId);
+        Comment comment = Comment.builder()
+            .content(request.content())
+            .episode(episode)
+            .member(member)
+            .build();
+        commentRepository.save(comment);
+
+        return CommentDTO.of(
+            comment.getId(),
+            comment.getContent(),
+            comment.getCreatedAt()
+        );
+    }
+
+    @Transactional
+    public void deleteComment(String commentId) {
+        Comment comment = commentRepository.findByIdAndDeletedAtIsNull(commentId)
+            .orElseThrow(() -> new EpisodeException(EpisodeErrorCode.COMMENT_NOT_FOUND));
+        commentRepository.delete(comment);
     }
 }
